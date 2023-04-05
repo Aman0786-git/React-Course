f786caa8b85ed30fee17ea61f1decf732380380a
Aman <aman999.au@gmail.com>
2023-04-05 16:21:41 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,55 @@
 import React, { useState } from "react";
-import Logo from "../assets/img/logo.jpg"
-const loggedIn =()=>{
-  // check if user is logged in
-  return false;
-}
+import Logo from "../assets/img/logo.jpg";
+import { Link } from "react-router-dom";
+
+// SPA - Single Page Application
+// Routing is of two types: 1. Client Side Routing 2. Server Side Routing
+// Client Side Routing
 
 const Title = () => (
   <a href="/">
-    <img
-      className="logo"
-      alt="logo"
-      src={Logo}
-    />
+    <img className="logo" alt="logo" src={Logo} />
   </a>
 );
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-
   return (
-
     <div className="header">
-    <Title />
-
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-        <li>Cart</li>
-      <li>
-        {
-          isLoggedIn ?
-          (<button className="logout-btn" onClick={()=>setIsLoggedIn(false)}>Logout</button>):
-          (<button className="login-btn" onClick={()=>setIsLoggedIn(true)}>Login</button>)
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
           
-        }
-        </li>
-      </ul>
+          <li>
+            {isLoggedIn ? (
+              <button
+                className="logout-btn"
+                onClick={() => setIsLoggedIn(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button className="login-btn" onClick={() => setIsLoggedIn(true)}>
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
-}
+  );
+};
 export default Header;
